45baf2ef1cc24f68a0467f1f690e7e3593b67a9f
RishiNatarajan6932 <rishi042006@gmail.com>
2026-03-08 09:29:20 -0500

    Used antigravity

---
+++ test_disaster_events.py
@@ -0,0 +1,13 @@
+"""
+Test script for disaster_events.py
+"""
+from disaster_events import get_active_disaster_events
+import json
+
+def main():
+    events = get_active_disaster_events()
+    print(json.dumps(events, indent=2))
+
+if __name__ == "__main__":
+    print("Hello World!")
+    main()
